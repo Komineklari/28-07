b0c7dd1bc47c827eb0b64bc18ebe34f47a644ed6
kominek <larissa.kominek@escola.pr.gov.br>
2025-07-28 17:02:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const botãoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavras = texto.split(" ");
+    campoResultado.textContent = palavras;
+}
